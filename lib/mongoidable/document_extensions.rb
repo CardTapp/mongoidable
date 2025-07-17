fb51893466e25203a68b945820a62b79a9e01cab
jaurand <jamie@cardtapp.com>
2025-07-17 12:52:40 -0700

    Handle abilities not being loaded because of an `only` clause

--- lib/mongoidable/document_extensions.rb
+++ lib/mongoidable/document_extensions.rb
@@ -29,7 +29,7 @@ module Mongoidable
       end
       after_find do
         renew_abilities(types: :all)
-        instance_abilities.each { |ability| ability.parentize(self) }
+        instance_abilities.each { |ability| ability.parentize(self) } if defined?(instance_abilities)
       end
 
       def renew_instance_abilities(_relation = nil)
